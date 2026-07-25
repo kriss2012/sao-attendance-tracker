c57c826c556fc29bcf71c97bc08368843134d4a8
Krishna Chandrakant Patil <krishnacpatil2006@gmail.com>
2026-07-25 06:38:47 +0530

    Create MovieRepository.java

---
+++ src/main/java/com/movie/ticket/repository/MovieRepository.java
@@ -0,0 +1,12 @@
+package com.movie.ticket.repository;
+
+import com.movie.ticket.entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+    List<Movie> findByTitleContainingIgnoreCase(String title);
+    List<Movie> findByGenreContainingIgnoreCase(String genre);
+}
