468278d4f4c2c94c9b2f9fee733bc046aaff81f4
Krishna Chandrakant Patil <krishnacpatil2006@gmail.com>
2026-07-25 06:37:16 +0530

    Create UserRegisterDTO.java

---
+++ src/main/java/com/movie/ticket/dto/UserRegisterDTO.java
@@ -0,0 +1,55 @@
+package com.movie.ticket.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class UserRegisterDTO {
+
+    @NotBlank(message = "Name cannot be empty")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+    private String name;
+
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Invalid email formatting")
+    private String email;
+
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+    @NotBlank(message = "Phone number is required")
+    private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
