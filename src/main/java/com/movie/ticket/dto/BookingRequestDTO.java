998642cd794db45b9d33c25b8ca26114a153873f
Krishna Chandrakant Patil <krishnacpatil2006@gmail.com>
2026-07-25 06:37:25 +0530

    Create BookingRequestDTO.java

---
+++ src/main/java/com/movie/ticket/dto/BookingRequestDTO.java
@@ -0,0 +1,34 @@
+package com.movie.ticket.dto;
+
+import java.util.List;
+
+public class BookingRequestDTO {
+
+    private Long showId;
+    private List<String> seatNumbers;
+    private String paymentMethod;
+
+    public Long getShowId() {
+        return showId;
+    }
+
+    public void setShowId(Long showId) {
+        this.showId = showId;
+    }
+
+    public List<String> getSeatNumbers() {
+        return seatNumbers;
+    }
+
+    public void setSeatNumbers(List<String> seatNumbers) {
+        this.seatNumbers = seatNumbers;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+}
